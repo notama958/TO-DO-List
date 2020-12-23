48d65f1d1042cb8103363ebc5fe87bedf3dfd1a3
Yen Tran <yen.o.tran@tuni.fi>
2020-12-23 22:06:02 +0200

    fix package.json

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,13 @@ const connectDb = require('../config/db');
 const express = require('express');
 const app = express();
 const path = require('path');
+// serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build')),
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+      res.sendFile(
+        path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+      );
     });
 }
 // mongo connect
@@ -14,8 +17,6 @@ connectDb();
 
 app.use(express.json({ extended: false })); // for route put
 app.use('/', require('./app'));
-
-// serve static assets in production
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
